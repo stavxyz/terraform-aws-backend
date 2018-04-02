f6c186b88dc104b4a5e385538c79b11f2f54bd38
Sam Stavinoha <smlstvnh@gmail.com>
2018-04-01 23:28:14 -0500

    outputs

---
+++ outputs.tf
@@ -0,0 +1,31 @@
+/*
+ * Module: tf_backend_aws
+ *
+ * Outputs:
+ *   - s3_backend_bucket_name
+ *   - dynamodb_lock_table_name
+ *   - dynamodb_lock_table_arn
+ *   - dynamodb_lock_table_stream_arn
+ *   - dynamodb_lock_table_stream_label
+ *
+ */
+
+output "s3_backend_bucket_name" {
+  value = "${aws_s3_bucket.tf_backend_bucket.id}"
+}
+
+output "dynamodb_lock_table_name" {
+  value = "${aws_dynamodb_table.tf_backend_state_lock_table.*.id}"
+}
+
+output "dynamodb_lock_table_arn" {
+  value = "${aws_dynamodb_table.tf_backend_state_lock_table.*.arn}"
+}
+
+output "dynamodb_lock_stream_arn" {
+  value = "${aws_dynamodb_table.tf_backend_state_lock_table.*.stream_arn}"
+}
+
+output "dynamodb_lock_stream_label" {
+  value = "${aws_dynamodb_table.tf_backend_state_lock_table.*.stream_label}"
+}
