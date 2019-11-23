15d3d8453f233305801da80149511ea8eb7c20ff
Sam Stavinoha <smlstvnh@gmail.com>
2019-11-22 20:15:39 -0600

    update variable types

--- variables.tf
+++ variables.tf
@@ -2,12 +2,14 @@ variable "backend_bucket" {
 }
 
 variable "dynamodb_lock_table_enabled" {
-  default     = 1
+  type        = bool
+  default     = true
   description = "Affects terraform-aws-backend module behavior. Set to false or 0 to prevent this module from creating the DynamoDB table to use for terraform state locking and consistency. More info on locking for aws/s3 backends: https://www.terraform.io/docs/backends/types/s3.html. More information about how terraform handles booleans here: https://www.terraform.io/docs/configuration/variables.html"
 }
 
 variable "dynamodb_lock_table_stream_enabled" {
-  default     = 0
+  type        = bool
+  default     = false
   description = "Affects terraform-aws-backend module behavior. Set to false or 0 to disable DynamoDB Streams for the table. More info on DynamoDB streams: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Streams.html. More information about how terraform handles booleans here: https://www.terraform.io/docs/configuration/variables.html"
 }
 
@@ -20,10 +22,12 @@ variable "dynamodb_lock_table_name" {
 }
 
 variable "lock_table_read_capacity" {
+  type    = number
   default = 1
 }
 
 variable "lock_table_write_capacity" {
+  type    = number
   default = 1
 }
 
