0be72d3b74f1bb296b5d2be990d0a2a142279ec2
Sunita Sahu <shnsh198@gmail.com>
2022-08-21 18:33:51 +0530

    deprication removed

--- examples/fresh-start/versions.tf
+++ examples/fresh-start/versions.tf
@@ -1,4 +1,11 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 1.2.0"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
 }
